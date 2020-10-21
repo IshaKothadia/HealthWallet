79f0af43e34749e8431d35ae0af1a47446bdcc0c
Vaman Patel <vamankumar456@gmail.com>
2020-10-21 11:28:39 +0530

    some files added

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ConstructionComponent } from './construction/construction.component';
+import { HomeComponent } from './home/home.component';
+import { LoginformComponent } from './loginform/loginform.component';
 import { SignupformComponent } from './signupform/signupform.component';
 
 const routes: Routes = [
-  { path: 'signup', component : SignupformComponent}
+  { path : '', redirectTo :'/home' , pathMatch:'full'},
+  { path : 'home', component : HomeComponent},
+  { path: 'signup', component : SignupformComponent},
+  { path : 'login', component : LoginformComponent}
+  { path : 'login', component : LoginformComponent},
+  { path : 'underwork', component : ConstructionComponent}
 ];
 
 @NgModule({
